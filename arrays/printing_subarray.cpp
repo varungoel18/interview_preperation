1a4cdaeb96a5767a145b5e2b5aa5ec7ecd610d61
Varun Goel <varun.varungoel.goel@gmail.com>
2022-02-08 22:48:15 +0530

    added one program

--- arrays/printing_subarray.cpp
+++ arrays/printing_subarray.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
+#include<algorithm>
+#include<climits>
 using namespace std;
-int main() {
-    int arr[] = {5,4,3,2,1};
-    int n = sizeof(arr)/sizeof(int);
+void subarray(int arr[] , int n) {
     for(int i=0;i<n;i++) {
         for(int j=i;j<n;j++) {
             for(int k=i;k<=j;k++) {
@@ -11,4 +11,30 @@ int main() {
             cout<<endl;
         }
     }
+}
+
+void max_sum(int arr[],int n) {
+    int max_sum = INT_MIN;
+    for(int i=0;i<n;i++) {
+        for(int j=i;j<n;j++) {
+            int sum = 0;
+            for(int k=i;k<=j;k++) {
+                sum += arr[k];
+                if(sum>max_sum) {
+                    max_sum = sum;
+                    cout<<arr[k]<<" ";
+                }
+            }
+        }
+    }
+    cout<<endl;
+    cout<<max_sum<<endl;
+}
+int main() {
+    int arr[] = {5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(int);
+    subarray(arr,n);
+    cout<<"max sum"<<endl;
+    max_sum(arr,n);
+    
 }
